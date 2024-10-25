538691ec9060f15d7d7c9dd693b027830ba441cc
Ronnie Samuel <samuelronnie55@gmail.com>
2024-10-25 14:20:59 -0400

    Updated tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
+    let currentPlayer = 'X';
     const squares = document.querySelectorAll('#board .square');
-    
+
     squares.forEach((square) => {
-        square.classList.add('square');
+        square.addEventListener('click', () => {
+            if (square.textContent === '') {
+                square.textContent = currentPlayer;
+                square.classList.add(currentPlayer);
+                currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+            }
+        });
     });
 });
